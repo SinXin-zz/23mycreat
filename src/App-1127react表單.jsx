c9bcf4d7557b7408d0fbc21b8e65423b4ea1ba54
SinXin-zz <nico4156@gmail.com>
2024-12-02 10:44:51 +0800

    1202

---
+++ src/App-1127react表單.jsx
@@ -0,0 +1,116 @@
+import { useState } from "react"
+
+function App() {
+    // 使用者名稱
+    const [inputUseerName, setInputUseerName] = useState("我是文字方塊");
+
+    // 縣市名
+    const [selCity, setSelCity] = useState("預設值:台北市");
+    const arrCity = ['台北市', '桃園市', '新北市'];
+
+    //閱讀確認
+    const [check, setCheck] = useState(false);
+
+    //複選 : 多個核取方塊
+    const [chkList, setChkList] = useState([]);
+
+    //建立函式處理被勾選的項目
+    const handleChkList = (e) => {
+        // console.log(e.target.value);
+        // 使用其餘運算子保留之前已經被勾選的項目+目前被勾選的項目
+        // setChkList([...chkList,e.target.value]);
+
+        //檢查是否被勾選
+        if (e.target.checked) {
+            //是 => 原本+新的
+            setChkList([...chkList, e.target.value]);
+        } else {
+            //否 => 從原本被勾選的項目中,拿掉不要的(filter)
+            setChkList(
+                chkList.filter((list) => {
+                    return list !== e.target.value
+                })
+            );
+        }
+    }
+
+    // 選項按鈕
+    
+
+
+    return (
+        <>
+            <h1>React-表單</h1>
+            <hr />
+            {/* input標籤 */}
+            <label htmlFor="username">使用者名稱</label>
+            <input type="text" id="username"
+                value={inputUseerName}
+                onChange={(e) => {
+                    setInputUseerName(e.target.value);
+                }}
+            />{inputUseerName}
+            <br />
+            {/* select */}
+            <label htmlFor="city">縣市名</label>
+            <select name="" id="city"
+                value={selCity}
+                onChange={(e) => {
+                    setSelCity(e.target.value);
+                }}>
+
+                {/*逐行寫法*/}
+                {/* 
+                <option value="台北市">台北市</option>
+                <option value="桃園市">桃園市</option>
+                <option value="新北市">新北市</option> */}
+
+
+                {/*讀取陣列寫法 在jsx寫js,記得包{}*/}
+                <option value="" disabled>請選擇</option>
+                {
+                    arrCity.map((city) => {
+                        return <option value={city} key={city}>{city}</option>
+                    })
+                }
+            </select>{selCity}
+            <br />
+
+            {/* 單一核取方塊 */}
+            <label htmlFor="isCheck">閱讀確認</label>
+            <input type="checkbox" id="isCheck"
+                value={check}
+                onChange={(e) => {
+                    //處理勾選值的變化
+                    setCheck(e.target.checked);
+                }} />{check.toString()}
+
+            <br />
+            {/* 複選 : 多個核取方塊 */}
+
+            <input type="checkbox" id="chkList1" name="like" value='HTML' onChange={handleChkList} />
+            <label htmlFor="chkList1">HTML</label>
+            <input type="checkbox" id="chkList2" name="like" value='CSS' onChange={handleChkList} />
+            <label htmlFor="chkList1">CSS</label>
+            <input type="checkbox" id="chkList3" name="like" value='JS' onChange={handleChkList} />
+            <label htmlFor="chkList1">JS</label>
+            <br />
+            {chkList}
+
+
+            <br />
+            {/*選項按鈕*/}
+            汽車駕照 <span id="car"></span>
+            <input type="radio" name="car" value="有" id="yes" onChange={(e)=>{
+                
+            }} />
+            <label htmlFor="yes">有</label>
+            <input type="radio" name="car" value="無" id="no"/>
+            <label htmlFor="no">無</label>
+
+            2.fu/3m6y8
+
+        </>
+    )
+}
+export default App
--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,81 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css";
 
 function App() {
-    // 使用者名稱
-    const [inputUseerName, setInputUseerName] = useState("我是文字方塊");
+    // 要用jason資料要寫在return前
+    useEffect(() => {
+        (async () => {
+            // const data = await axios.get('./F-C0032-001.json');
+            const data = await axios.get('./F-C0032-001.json');
+            const { location } = data.data.cwaopendata.dataset;
+            console.log(location);
 
-    // 縣市名
-    const [selCity, setSelCity] = useState("預設值:台北市");
-    const arrCity = ['台北市', '桃園市', '新北市'];
+        })
+    }, [])
 
-    //閱讀確認
-    const [check, setCheck] = useState(false);
-
-    //複選 : 多個核取方塊
-    const [chkList, setChkList] = useState([]);
-
-    //建立函式處理被勾選的項目
-    const handleChkList = (e) => {
-        // console.log(e.target.value);
-        // 使用其餘運算子保留之前已經被勾選的項目+目前被勾選的項目
-        // setChkList([...chkList,e.target.value]);
-
-        //檢查是否被勾選
-        if (e.target.checked) {
-            //是 => 原本+新的
-            setChkList([...chkList, e.target.value]);
-        } else {
-            //否 => 從原本被勾選的項目中,拿掉不要的(filter)
-            setChkList(
-                chkList.filter((list) => {
-                    return list !== e.target.value
-                })
-            );
-        }
-    }
 
     return (
         <>
-            <h1>React-表單</h1>
-            <hr />
-            {/* input標籤 */}
-            <label htmlFor="username">使用者名稱</label>
-            <input type="text" id="username"
-                value={inputUseerName}
-                onChange={(e) => {
-                    setInputUseerName(e.target.value);
-                }}
-            />{inputUseerName}
-            <br />
-            {/* select */}
-            <label htmlFor="city">縣市名</label>
-            <select name="" id="city"
-                value={selCity}
-                onChange={(e) => {
-                    setSelCity(e.target.value);
-                }}>
+            <h2>36小時天氣預報</h2>
+            <div className="col1">
+                <div className="taipei">
+                    <h3>台北市</h3>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                </div>
 
-                {/*逐行寫法*/}
-                {/* 
-                <option value="台北市">台北市</option>
-                <option value="桃園市">桃園市</option>
-                <option value="新北市">新北市</option> */}
+                <div className="newtaipei">
+                    <h3>新北市</h3>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                    <div className="dayWeather">
+                        <h4>2日</h4>
+                        <p>上午6:00</p>
+                        <p>~</p>
+                        <p>下午6:00</p>
+                        <figure><img src="./public/weatherIcon/多雲.svg" alt="" /></figure>
+                        <p>晴時多雲</p>
+                    </div>
+                </div>
+            </div>
 
-
-                {/*讀取陣列寫法 在jsx寫js,記得包{}*/}
-                <option value="" disabled>請選擇</option>
-                {
-                    arrCity.map((city) => {
-                        return <option value={city} key={city}>{city}</option>
-                    })
-                }
-            </select>{selCity}
-            <br />
-
-            {/* 單一核取方塊 */}
-            <label htmlFor="isCheck">閱讀確認</label>
-            <input type="checkbox" id="isCheck"
-                value={check}
-                onChange={(e) => {
-                    //處理勾選值的變化
-                    setCheck(e.target.checked);
-                }} />{check.toString()}
-
-            <br />
-            {/* 複選 : 多個核取方塊 */}
-
-            <input type="checkbox" id="chkList1" name="like" value='HTML' onChange={handleChkList} />
-            <label htmlFor="chkList1">HTML</label>
-            <input type="checkbox" id="chkList2" name="like" value='CSS' onChange={handleChkList} />
-            <label htmlFor="chkList1">CSS</label>
-            <input type="checkbox" id="chkList3" name="like" value='JS' onChange={handleChkList} />
-            <label htmlFor="chkList1">JS</label>
-            <br />
-            {chkList}
 
         </>
     )
